de67430a4814c77f6aa6324415933bc8fe68b361
Eliot <etroop719@gmail.com>
2018-10-06 10:45:51 -0700

    First MonoGame Project Initial Commit

---
+++ intro/intro/Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace intro
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        ScreenSize screenSize;
+
+        Sprite sprite;
+        int xSpeed = 1;
+        int ySpeed = 1;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        
+        protected override void Initialize()
+        {
+            screenSize.Width = GraphicsDevice.Viewport.Width;
+            screenSize.Height = GraphicsDevice.Viewport.Height;
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+           
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+
+            sprite = new Sprite(Content.Load<Texture2D>("ball"), new Vector2(0, 0), Color.White);
+       
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            sprite.Update(screenSize);
+            base.Update(gameTime);
+        }
+
+    
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+
+            sprite.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ intro/intro/Sprite.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace intro
+{
+    public struct ScreenSize
+    {
+        public int Width;
+        public int Height;
+    }
+
+    public class Sprite
+    {
+       public Texture2D image;
+        public Vector2 position;
+        Color tint;
+        int xSpeed = 1;
+        int ySpeed = 1;
+        public Sprite(Texture2D image, Vector2 position, Color tint)
+        {
+            this.image = image;
+            this.position = position;
+            this.tint = tint;
+            
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, position, tint);
+        }
+        public void Update(ScreenSize screenSize)
+        {
+            if (position.X < 0)
+            {
+                xSpeed = 1;
+            }
+            if (position.X + image.Width > screenSize.Width)
+            {
+                xSpeed = -1;
+            }
+            if (position.Y < 0)
+            {
+                ySpeed = 1;
+            }
+            if (position.Y + image.Height > screenSize.Height)
+            {
+                ySpeed = -1;
+            }
+
+            position.X += 5 * xSpeed;
+            position.Y += 5 * ySpeed;
+        }
+
+    }
+}
